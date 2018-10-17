a078c719435f7183c04d8b56f3bb4dc3304a23d9
Guilherme Braga <gbp1999@gmail.com>
2018-10-17 11:56:26 -0300

    Add files via upload

--- questao2.py
+++ questao2.py
@@ -7,13 +7,14 @@ import copy
 
 from rgb_ycbcr import rgb_para_ycbcr
 from ycbcr_rgb import ycbcr_para_rgb
+from normalized_cross_corelation import correlation
 
 # leio a imagem final
 imagem_de_trabalho = cv2.imread("imagem_final.bmp")
 
 # passo para YCbCr
 imagem_ycbcr = copy.copy(imagem_de_trabalho)
-rgb_para_ycbcr(imagem_de_trabalho)
+rgb_para_ycbcr(imagem_ycbcr)
 
 # retiro o que necessito
 altura, largura, channels = imagem_ycbcr.shape
@@ -21,4 +22,17 @@ altura, largura, channels = imagem_ycbcr.shape
 # retorno que tudo deu certo
 print("Imagem lida com sucesso!")
 
-aviao_exemplo =
+# Recortaremos da imagem um exemplo de avião
+# Fui no Paint e escolhi as coordenadas de um avião, no meu caso, o segundo avião da esquerda para a direita
+
+# a primeira coordenada representa o canto inferior direito da imagem
+# a segunda coordenada representa o canto superior esquerdo
+# a linha reta entre essas duas coordenadas será a diagonal da imagem
+
+aviao_exemplo = imagem_ycbcr[601:610, 521:504, 0]
+altura_aviao, largura_aviao, channels_aviao = imagem_ycbcr.shape
+
+for y in range(altura - altura_aviao):
+    for x in range(largura - largura_aviao):
+        if correlation(imagem_ycbcr[y:y+altura_aviao, x:x+largura_aviao], aviao_exemplo) == 0:
+            print("ZAP")
